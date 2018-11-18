9e43b531d4697e4edb2faca863f93295216b1b47
Paul Baker <paul.baker@wgu.edu>
2018-11-18 03:05:30 -0700

    Explicitly adding kotlin. It's already there, but I need to trigger a snapshot and prefer this to be more obvious anyway

--- src/main/kotlin/io/paulbaker/integration/qtest/rest/Common.kt
+++ src/main/kotlin/io/paulbaker/integration/qtest/rest/Common.kt
@@ -3,13 +3,16 @@ package io.paulbaker.integration.qtest.rest
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 /**
  * Registers the Kotlin module, but we also want the Jdk8 and JavaTime module.
  */
 val objectMapper: ObjectMapper = jacksonObjectMapper().registerModules(
+        KotlinModule(),
         Jdk8Module(),
         JavaTimeModule()
 )
