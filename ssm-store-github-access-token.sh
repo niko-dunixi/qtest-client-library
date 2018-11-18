a9a11fce042d215e4e095fb677569963968c910b
Paul Baker <paul.baker@wgu.edu>
2018-11-17 23:31:57 -0700

    Parameterizing test values

--- ssm-store-github-access-token.sh
+++ ssm-store-github-access-token.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+source ./sourceable-variables.sh
+
 echo -n "Enter your GitHub personal access token: "
 read github_token
 
-aws --profile paulbaker ssm put-parameter \
+aws ssm put-parameter \
   --name "/qtest-client-library/github-personal-access-token" \
   --description "The token that allows the us to access the github" \
   --type String \
   --value "${github_token}" \
-  "${@}" # We add this to pass any extra values to aws-cli, eg --overwrite
+  --overwrite
---
+++ ssm-store-qtest-values.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+
+source ./sourceable-variables.sh
+
+echo -n "Enter your QTest subdomain: "
+read qtest_subdomain
+echo -n "Enter your QTest username: "
+read qtest_username
+echo -n "Enter your QTest password: "
+read -s qtest_password
+
+printf "\nUploading values into AWS SSM\n"
+
+aws ssm put-parameter \
+  --name "/qtest-client-library/integration-test/subdomain" \
+  --description "The subdomain that is used with qtest" \
+  --type String \
+  --value "${qtest_subdomain}" \
+  --overwrite
+
+aws ssm put-parameter \
+  --name "/qtest-client-library/integration-test/username" \
+  --description "The username that is used with qtest" \
+  --type String \
+  --value "${qtest_username}" \
+  --overwrite
+
+aws ssm put-parameter \
+  --name "/qtest-client-library/integration-test/password" \
+  --description "The password that is used with qtest" \
+  --type String \
+  --value "${qtest_password}" \
+  --overwrite
