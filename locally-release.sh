055789fad8c198162fdfd5144cc3f19d4cc29001
Paul Baker <paul.baker@wgu.edu>
2018-11-18 12:21:04 -0700

    Removing skip tests

--- locally-release.sh
+++ locally-release.sh
@@ -10,13 +10,13 @@ source ./sourceable-variables.sh
 source ./sourceable-ssm-qtest-values.sh
 
 # Validate the release
-mvn clean test
+mvn clean package
 
 # Now that we know it is working, we can skip the tests because they will be run many
 # times, redundantly, otherwise. We can now roleback on release failure as well
 trap rollback ERR
 
 # Release
-mvn release:prepare release:perform -Dmaven.test.skip=true
+mvn release:prepare release:perform
 # Push the most recent snapshot
-mvn deploy -Dmaven.test.skip=true
+mvn deploy
